25c75db52fcd248c78afb6bb8393c66233c452f9
= <akhdakhz@gmail.com>
2018-04-22 11:14:51 +0700

    Create Sidebar & Navbar

---
+++ src/main/java/com/apap/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.apap.controller;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Primary
+public class MainController {
+
+    @RequestMapping("/")
+    public String index ()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/literatur/tambah")
+    public String tambahliteratur ()
+    {
+        return "Literatur/tambahliteratur";
+    }
+}
---
+++ src/main/java/com/apap/siperpus/SiperpusApplication.java
@@ -0,0 +1,12 @@
+package com.apap.siperpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiperpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiperpusApplication.class, args);
+	}
+}
