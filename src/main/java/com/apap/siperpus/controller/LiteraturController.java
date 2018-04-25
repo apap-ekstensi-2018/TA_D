7ab27ba71dd76a9a84526d668b93839b3e366a1f
KurniawanHendiW <kurniawanhendiwijaya@gmail.com>
2018-04-25 19:08:33 +0700

    Form menambahkan literatur

--- src/main/java/com/apap/siperpus/controller/LiteraturController.java
+++ src/main/java/com/apap/siperpus/controller/LiteraturController.java
@@ -1,7 +1,9 @@
 package com.apap.siperpus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created on : April 24, 2018
@@ -11,7 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LiteraturController {
     @RequestMapping("/literatur/tambah")
-    public String tambahliteratur () {
+    public String tambah() {
         return "Literatur/tambahliteratur";
+    }
+
+    @RequestMapping(value = "/literatur/tambah", method = RequestMethod.POST)
+    public String submitTambah(Model model) {
+        return null;
     }
 }
