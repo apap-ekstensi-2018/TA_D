0eb255eccca16414ab782bd90bba27c314ce7b8a
ganigemilar <ganigemilar10@gmail.com>
2018-04-29 22:17:23 +0700

    delete import MahasiswaMapper

--- src/main/java/com/apap/siperpus/dao/MahasiswaMapper.java
+++ src/main/java/com/apap/siperpus/dao/MahasiswaMapper.java
@@ -3,8 +3,6 @@ package com.apap.siperpus.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.apap.siperpus.model.MahasiswaModel;
