f5a2d1c8bf7a6cab582bc59cd12146e26dffef34
ibnvakbar <r.ibnuakbar@gmail.com>
2018-04-30 16:00:01 +0700

    fix form ubahLiteratur view

--- src/main/java/com/apap/siperpus/dao/LiteraturMapper.java
+++ src/main/java/com/apap/siperpus/dao/LiteraturMapper.java
@@ -1,10 +1,7 @@
 package com.apap.siperpus.dao;
 
 import com.apap.siperpus.model.LiteraturModel;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -17,6 +14,9 @@ import java.util.List;
 public interface LiteraturMapper {
     @Select("SELECT * FROM literatur")
     List<LiteraturModel> selectAllLiteratur ();
+
+    @Select("SELECT id,judul,penulis,penerbit,jenis_literatur,jumlah,status from literatur where id=#{id}")
+    LiteraturModel selectLiteratur(@Param("id") int id);
 
     @Update("Update student set judul = #{literatur.judul}," +
             "penulis = #{literatur.penulis}," +
