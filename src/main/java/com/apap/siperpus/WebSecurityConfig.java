dbdac844f4c10e98aaedaa4fe2a024da92ce2628
= <akhdakhz@gmail.com>
2018-05-01 11:06:18 +0700

    create login page

--- src/main/java/com/apap/siperpus/WebSecurityConfig.java
+++ src/main/java/com/apap/siperpus/WebSecurityConfig.java
@@ -1,18 +1,25 @@
 package com.apap.siperpus;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource dataSource;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*http
+		http
 		.authorizeRequests()
 		.antMatchers("/resources/**").permitAll()
 		.antMatchers("/").permitAll()
@@ -20,18 +27,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.authenticated()
 		.and()
 		.formLogin()
-		.loginPage("/login").permitAll()
+		.loginPage("/login").defaultSuccessUrl("/").permitAll()
 		.and()
 		.logout()
-		.permitAll();*/
+		.permitAll();
+	}
+	@Autowired
+	public void configureAuthentication(AuthenticationManagerBuilder auth) throws
+			Exception{
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery(
+						"select username, password, 1 from user_account " +
+								"where username=?")
+				.authoritiesByUsernameQuery(
+						"select username, role from user_account " +
+								"where username=?");
 	}
 
-	@Autowired
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+				.antMatchers("/bootstrap/**")
+				.antMatchers("/css/**")
+				.antMatchers("/dist/**")
+				.antMatchers("/plugins/**");
+	}
+	/*@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 		.inMemoryAuthentication()
 		.withUser("admin")
 		.password("admin")
 		.roles("ADMIN");
+	}*/
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 }
