35a47052e66c02e95727a44789f964c350c0fb44
bw <herlen.adita@kokatto.com>
2018-05-05 16:44:42 +0700

    Add viewall literatur

--- src/main/java/com/apap/siperpus/controller/LiteraturController.java
+++ src/main/java/com/apap/siperpus/controller/LiteraturController.java
@@ -1,7 +1,15 @@
 package com.apap.siperpus.controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.apap.siperpus.model.LiteraturModel;
+import com.apap.siperpus.service.LiteraturService;
 
 /**
  * Created on : April 24, 2018
@@ -10,8 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class LiteraturController {
+	@Autowired
+	LiteraturService literaturDAO;
+	
     @RequestMapping("/literatur/tambah")
     public String tambahliteratur () {
         return "Literatur/tambahliteratur";
+    }
+    
+    @RequestMapping("/literatur/viewall")
+    public String lihatDaftarLiteratur(Model model) {
+    		List<LiteraturModel> literaturs = literaturDAO.selectAllLiteratur();
+        model.addAttribute ("literaturs", literaturs);
+        return "Literatur/daftarLiteratur";
+        
+    		
     }
 }
