9971b29c0c774aaadef4d9935d1f290a2f216c70
ganigemilar <ganigemilar10@gmail.com>
2018-05-05 15:37:30 +0700

    update wsc and fragment

--- src/main/java/com/apap/siperpus/WebSecurityConfig.java
+++ src/main/java/com/apap/siperpus/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/resources/**").permitAll()
+		.antMatchers("/","/resources/**").permitAll()
 		.antMatchers("/**").hasRole(ROLE_STAFF)
 		.antMatchers("/**").hasAnyRole(ROLE_DOSEN, ROLE_MAHASISWA)
 		.anyRequest().authenticated()
