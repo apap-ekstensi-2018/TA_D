906d40f3bd5c34bbef3dfb4ee905c832f3b3d083
ganigemilar <ganigemilar10@gmail.com>
2018-05-08 20:19:24 +0700

    update upload file

--- src/main/java/com/apap/siperpus/controller/LiteraturController.java
+++ src/main/java/com/apap/siperpus/controller/LiteraturController.java
@@ -1,10 +1,16 @@
 package com.apap.siperpus.controller;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.apap.siperpus.model.LiteraturModel;
 import com.apap.siperpus.service.LiteraturService;
 import org.json.JSONArray;
@@ -25,6 +31,8 @@ import com.apap.siperpus.service.LiteraturService;
 @Controller
 @RequestMapping("/literatur")
 public class LiteraturController {
+	private static final String PATH_FILE_UPLOAD = "//pdf//";
+	
 	@Autowired
 	LiteraturService literaturDAO;
     
@@ -67,7 +75,21 @@ public class LiteraturController {
     }
     
     @RequestMapping("/upload")
-    public String uploadKaryaTulis() {
+    public String uploadKaryaTulis(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+    	if (!file.isEmpty()) {
+    		try {	
+	    		byte[] bytes = file.getBytes();
+				Path path = Paths.get(PATH_FILE_UPLOAD + file.getOriginalFilename());
+				Files.write(path, bytes);
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+		}
     	return "Literatur/uploadKaryaTulis";
     }
+    
+    /*@RequestMapping("/upload/submit")
+    public String submitKaryaTulis(@Para) {
+    	return "";
+    }*/
 }
